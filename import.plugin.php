aa78a2aaafd541400e30200ed7d91dc7a5b29eca
chrismeller <chrismeller@f4a90942-1786-4aa1-b092-57c31935ddc3>
2010-10-15 20:41:49 +0000

    Add the other required method and make sure we add ourselves to the import list.
    git-svn-id: https://svn.habariproject.org/habari-extras/plugins/import/trunk@3387 f4a90942-1786-4aa1-b092-57c31935ddc3

--- import.plugin.php
+++ import.plugin.php
@@ -2,9 +2,29 @@
 
 	class Import extends Plugin implements Importer {
 		
+		private $import_name = 'BlogML XML File';
+		
 		public function action_update_check ( ) {
 			
 			Update::add( 'Import', '7c4133b7-6578-4dc0-9dbe-f8ef89a9ec80', $this->info->version );
+			
+		}
+		
+		public function filter_import_names ( $import_names ) {
+			
+			$import_names[] = _t( $this->import_name );
+			
+			return $import_names;
+			
+		}
+		
+		public function filter_import_stage ( $stageoutput, $import_name, $stage, $step ) {
+			
+			// only run if we're the importer being used
+			if ( _t( $this->import_name ) != $import_name ) {
+				// must return $stageoutput as it could contain the output of another plugin!
+				return $stageoutput;
+			}
 			
 		}
 		
